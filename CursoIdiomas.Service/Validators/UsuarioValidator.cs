93d02dbbb637aba6333680679dd16e72e1097dd9
Mateus Machado <mateusmmachado2@gmail.com>
2021-07-26 20:54:17 -0300

    Adiciona serviços de Usuário
    Adiciona classe base de serviço e validator de Usuário

---
+++ CursoIdiomas.Service/Validators/UsuarioValidator.cs
@@ -0,0 +1,28 @@
+﻿using CursoIdiomas.Domain.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoIdiomas.Service.Validators
+{
+    public class UsuarioValidator : AbstractValidator<Usuario>
+    {
+        public UsuarioValidator()
+        {
+            RuleFor(c => c.Nome)
+                .NotEmpty().WithMessage("Informe um nome!")
+                .NotNull().WithMessage("Informe um nome!");
+
+            RuleFor(c => c.Email)
+                .NotEmpty().WithMessage("Informe um email!")
+                .NotNull().WithMessage("Informe um email!");
+
+            RuleFor(c => c.Senha)
+                .NotEmpty().WithMessage("Informe uma senha!")
+                .NotNull().WithMessage("Informe uma senha!");
+        }
+    }
+}
