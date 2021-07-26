2764e7954170add105b5665a0e6c5745e814efc5
Mateus Machado <mateusmmachado2@gmail.com>
2021-07-26 20:23:59 -0300

    Adiciona repositório base

---
+++ CursoIdiomas.Infrastructure/Repository/BaseRepository.cs
@@ -0,0 +1,49 @@
+﻿using CursoIdiomas.Domain.Entities;
+using CursoIdiomas.Domain.Interfaces;
+using CursoIdiomas.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoIdiomas.Infrastructure.Repository
+{
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : BaseEntity
+    {
+        protected readonly SqlContext _context;
+
+        public BaseRepository(SqlContext context)
+        {
+            _context = context;
+        }        
+        
+        public void Delete(int id)
+        {
+            _context.Set<TEntity>().Remove(Select(id));
+            _context.SaveChanges();
+        }
+
+        public void Insert(TEntity obj)
+        {
+            _context.Set<TEntity>().Add(obj);
+            _context.SaveChanges();
+        }
+
+        public IList<TEntity> Select()
+        {
+            return _context.Set<TEntity>().ToList();
+        }
+
+        public TEntity Select(int id)
+        {
+            return _context.Set<TEntity>().Find(id);
+        }
+
+        public void Update(TEntity obj)
+        {
+            _context.Entry(obj).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+        }
+    }
+}
