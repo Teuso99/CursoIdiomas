c68c3300b296e8205dfea018877c76bcb475fb84
Mateus Machado <mateusmmachado2@gmail.com>
2021-07-26 19:53:01 -0300

    Adiciona DbContext e mapping de Usuário

---
+++ CursoIdiomas.Infrastructure/Context/SqlContext.cs
@@ -0,0 +1,28 @@
+﻿using CursoIdiomas.Domain.Entities;
+using CursoIdiomas.Infrastructure.Mapping;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoIdiomas.Infrastructure.Context
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Usuario>(new UsuarioMap().Configure);
+        }
+    }
+}
---
+++ CursoIdiomas.Infrastructure/Mapping/UsuarioMap.cs
@@ -0,0 +1,40 @@
+﻿using CursoIdiomas.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoIdiomas.Infrastructure.Mapping
+{
+    public class UsuarioMap : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.ToTable("Usuario");
+
+            builder.HasKey(prop => prop.Id);
+
+            builder.Property(prop => prop.Nome)
+                .HasConversion(prop => prop.ToString(), prop => prop)
+                .IsRequired()
+                .HasColumnName("Nome")
+                .HasColumnType("varchar(100)");
+
+            builder.Property(prop => prop.Email)
+                .HasConversion(prop => prop.ToString(), prop => prop)
+                .IsRequired()
+                .HasColumnName("Email")
+                .HasColumnType("varchar(100)");
+
+            builder.Property(prop => prop.Senha)
+                .HasConversion(prop => prop.ToString(), prop => prop)
+                .IsRequired()
+                .HasColumnName("Senha")
+                .HasColumnType("varchar(100)");
+        }
+    }
+}
