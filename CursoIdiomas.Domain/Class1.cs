970c3b73c4fafb66ef5cde423a246ac2a3223b0e
Mateus Machado <mateusmmachado2@gmail.com>
2021-07-26 19:22:51 -0300

    Adiciona entidade Usuário
    Adiciona a entidade base que atribuí o id para todas as outras entidades e a entidade Usuário, junto também das interfaces padrão de repositório e serviço.

--- CursoIdiomas.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CursoIdiomas.Domain
-{
-    public class Class1
-    {
-    }
-}
--- CursoIdiomas.Service/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CursoIdiomas.Service
-{
-    public class Class1
-    {
-    }
-}
