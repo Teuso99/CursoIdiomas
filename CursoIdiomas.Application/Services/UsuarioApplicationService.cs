ccc4bd5336ebb6230acd800123c2c98335280acb
Mateus Machado <mateusmmachado2@gmail.com>
2021-08-14 18:57:32 -0300

    Adiciona autenticação no application service de usuário

--- CursoIdiomas.Application/Services/UsuarioApplicationService.cs
+++ CursoIdiomas.Application/Services/UsuarioApplicationService.cs
@@ -9,10 +9,17 @@ namespace CursoIdiomas.Application.Services
     public class UsuarioApplicationService :
         BaseApplicationService<Usuario, UsuarioDTO>, IUsuarioApplicationService
     {
+        private readonly IUsuarioService _usuarioService;
+
         public UsuarioApplicationService(IMapper mapper, IUsuarioService service) :
             base(mapper, service)
         {
+            _usuarioService = service;
+        }
 
+        public string Authenticate(Usuario entity)
+        {
+            return _usuarioService.Authenticate(entity);
         }
     }
 }
