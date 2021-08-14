91567180a41713eb99711685e9d396d75ce7b9c3
Mateus Machado <mateusmmachado2@gmail.com>
2021-08-14 20:37:51 -0300

    Adiciona camada de Apresentação

---
+++ CursoIdiomas.Presentation.Api/Controllers/AlunoController.cs
@@ -0,0 +1,17 @@
+﻿using CursoIdiomas.Application.DTOs;
+using CursoIdiomas.Application.Interfaces;
+using CursoIdiomas.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CursoIdiomas.Presentation.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : BaseController<Aluno, AlunoDTO>
+    {
+        public AlunoController(IAlunoApplicationService service) : base(service)
+        {
+
+        }
+    }
+}
